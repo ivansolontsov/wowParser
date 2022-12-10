a3f7d31880f0e0172980b05f4e15155e779947b3
Ivan Solontsov <ivansolontsov@yandex.ru>
2022-12-10 18:49:53 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wowParser/precache-manifest.2e2d1f04f73f7870d0c36d5f862d4e7e.js"
+  "/wowParser/precache-manifest.1eaa6177abd29a69096ed1de041640d7.js"
 );
 
 workbox.clientsClaim();
